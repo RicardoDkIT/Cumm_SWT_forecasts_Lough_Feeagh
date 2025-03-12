9c141f8500ee1fa1818e1305061c619850d6e38e
Ricardo.DkIT <ricardo.marroquinpaiz@dkit.ie>
2025-03-12 16:59:33 +0000

    Updated new script to generate cummulative temperature forecasts

--- Script_to_Access_Lake_Feeagh_Data.R
+++ Script_to_Access_Lake_Feeagh_Data.R
@@ -1,5 +1,6 @@
-# Script to access Lake Feeagh data
-# Modified: 07 October 2024
+# Script to compute cummpulative water temperature in degrees C per day in Lough Feeagh
+# This script is largely based on the Ricardo Paiz script that access Feeagh data through rerddap
+# Modified: 12 March 2025
 # Ricardo Paiz
 
 ##Packages######################################################################
@@ -48,64 +49,26 @@ fgh$time <- gsub('Z','',fgh$time) #Remove the Z
 
 #Formatting to datetime (checking in case there is other format in the dataset)
 if(!is.na(as.POSIXct(fgh$time[nrow(fgh)], format = '%d/%m/%Y %H:%M:%S'))){
-fmt = '%Y-%m-%d %H:%M:%S'
+  fmt = '%Y-%m-%d %H:%M:%S'
 }else if(!is.na(as.POSIXct(fgh$time[1], format = '%Y-%m-%d %H:%M:%S'))){
-fmt = '%Y-%m-%d %H:%M:%S'
+  fmt = '%Y-%m-%d %H:%M:%S'
 }
 
 str(fgh)
 
-#Checking for NAs in time column (provided - external)##########################
-
-fgh$time <- as.POSIXct(fgh$time, format = fmt, tz = 'UTC') #Format into datetime
-date.na = sum(is.na(fgh$time))
-print(paste0('Start: ', range(fgh$time, na.rm = T)[1], '; End: ', range(fgh$time, na.rm = T)[2]))
-print(paste('No. of NAs in time:',date.na))
-
-#Check timestep (provided - external)###########################################
-
-print(dim(fgh))
-print(summary(fgh$time))
-dates = seq(from = range(fgh$time)[1],to = range(fgh$time)[2], by =120)
-dif <- as.numeric(fgh[2:nrow(fgh),'time']-fgh[1:(nrow(fgh)-1),'time'])
-sam <- fgh[order(fgh[,'time']),]
-if(length(dates) != nrow(fgh)){
-df = data.frame(time = dates,
-test =rep(NA,length(dates)), 
-stringsAsFactors=FALSE) 
-df = merge(df,sam, by ='time', all.x = T)
-df$test <- NULL
-fgh <- df
-print('Filled in missing dates with NAs')
-}
-dif2 <- fgh[2:nrow(fgh),'time']-fgh[1:(nrow(fgh)-1),'time']
-if(max(dif)>2 | min(dif) < 2){
-par(mfrow=c(2,1))
-plot(dif, main = 'Time Difference - Raw', ylab = 'sec')
-plot(dif2, main = 'Time Difference - Corrected', ylab = 'sec')
-print('Timestep has been corrected')
-}
-sam <- NULL
-if(date.na != 0){
-snd.na = sum(is.na(fgh$Sonde_Temperature))
-anem.na = sum(is.na(fgh$Anemometer))
-if(date.na == snd.na & date.na == anem.na){
-dates = seq(fgh$time[1], fgh$time[nrow(fgh)], by = 120)
-}
-}
 
 #Replace Nan with NA (provided - external)######################################
 for(i in 2:ncol(fgh)){
-n = which(is.nan(fgh[,i]))
-#print(n) ##To check the file
-#}
-if(length(n) == 0){
-print(paste('No NaN in',colnames(fgh)[i]))
-next
-}else{
-fgh[n,i] <- NA
-print(paste('NaN replaced with NAs in',colnames(fgh)[i]))
-}
+  n = which(is.nan(fgh[,i]))
+  #print(n) ##To check the file
+  #}
+  if(length(n) == 0){
+    print(paste('No NaN in',colnames(fgh)[i]))
+    next
+  }else{
+    fgh[n,i] <- NA
+    print(paste('NaN replaced with NAs in',colnames(fgh)[i]))
+  }
 }
 
 str(fgh)
@@ -124,7 +87,7 @@ data_daily
 colnames(data_daily) <- c("datetime","0.9","2","5","8","11","14","16","18","20","22","27","32","42")
 
 for(o in 1:(no_days)){
-
+  
   data_daily[o,1] <- as.character(start + o -1, format = '%Y-%m-%d %H:%M:%S' )
   print(data_daily[o,1])
   
@@ -133,7 +96,7 @@ for(o in 1:(no_days)){
 for(t in 1:(no_days)){
   
   day_calc <- fgh[fgh$time >= start + t -1 & fgh$time  < start + t ,]
-
+  
   print(day_calc[1,1])
   
   for(z in 2:ncol(fgh)){
@@ -170,7 +133,7 @@ output_daily[,4][output_daily[,4] > output_daily[,2]*1.02] <- NA
 output_daily[,4][output_daily[,4] > output_daily[,3]*1.02] <- NA
 
 output_daily[,4][output_daily[,4] < output_daily[,3]-0.7] <- NA #additional check - temperatures cant vary more than 0.95 percentile of variations 2004-2023
-   
+
 # sensor at 8 m
 output_daily[,5][output_daily[,5] > output_daily[,2]*1.02] <- NA
 output_daily[,5][output_daily[,5] > output_daily[,3]*1.02] <- NA
@@ -294,107 +257,115 @@ output_daily[,14][output_daily[,14] > output_daily[,13]*1.02] <- NA
 output_daily[,14][output_daily[,14] < output_daily[,13]-2.0] <- NA #additional check - temperatures cant vary more than 0.96 percentile of variations 2004-2023. This was 0.9 but 2.2 (previous percetile was greater)
 
 
+##Code to compute cummulative temperature based solely on sonde temperature
+##This was decided as other sensors could be missreading
+
+Past_sonde_observations <- read.csv("Past_sonde_observations_since_2023.csv", header = TRUE)
+
+# Ensure datetime is in the correct date format first
+Past_sonde_observations$datetime <- as.Date(Past_sonde_observations$datetime, format = "%m/%d/%Y")
+
+# Now convert to the desired format "%m/%d/%Y" if needed
+Past_sonde_observations$datetime <- format(Past_sonde_observations$datetime, "%m/%d/%Y")
+
+Base_data <- output_daily[, c(1, 2)]
+Base_data$datetime <- format(as.Date(Base_data$datetime), "%m/%d/%Y")
+colnames(Base_data) <- colnames(Past_sonde_observations)
+
+# Add the rows from Base_data to Past_sonde_observations
+merged_data <- rbind(Past_sonde_observations, Base_data)
+
+merged_data <- merged_data[!duplicated(merged_data$datetime), ]
+
+merged_data$Temp_for_cumm_T <- merged_data$observation
+
+# Set Temp_for_cumm_T to observation, with 0 for 1st September
+merged_data$Temp_for_cumm_T <- merged_data$observation
+#merged_data$Temp_for_cumm_T[format(as.Date(merged_data$datetime, format = "%m/%d/%Y"), "%m-%d") == "09-01"] <- 0
+
+# Replace NAs in Temp_for_cumm_T with the previous available observation value
+library(zoo)
+merged_data$Temp_for_cumm_T <- na.locf(merged_data$Temp_for_cumm_T, na.rm = FALSE)
 
 
-#FLARE target configuration#####################################################
+# Function to calculate calendar day starting from 1st September
+get_calendar_day <- function(date) {
+  year <- as.numeric(format(date, "%Y"))  # Convert year to numeric
+  start_date <- as.Date(paste0(year, "-09-01"))  # 1st September of the current year
+  end_date <- as.Date(paste0(year + 1, "-08-31"))  # 31st August of the next year
+  
+  # If the date is before 1st September, adjust to the previous year's cycle
+  if (date < start_date) {
+    start_date <- as.Date(paste0(year - 1, "-09-01"))
+    end_date <- as.Date(paste0(year, "-08-31"))
+  }
+  
+  # Calculate the calendar day
+  calendar_day <- as.numeric(difftime(date, start_date, units = "days")) + 1
+  return(calendar_day)
+}
 
-num_rows <- ((ncol(output_daily)-1)*no_days)
-data_matrix <- matrix(NA, nrow = num_rows, ncol = 5)
-FLARE_dataset <- data.frame(data_matrix)
+# Apply the function to the 'datetime' column in merged_data
+merged_data$calendar_day <- sapply(as.Date(merged_data$datetime, format = "%m/%d/%Y"), get_calendar_day)
 
-colnames(FLARE_dataset) <- c("datetime","site_id","depth","observation","variable")
 
-#Column1: datetime##############################################################
+write.csv(merged_data[, 1:2], "Past_sonde_observations_since_2023.csv", row.names = FALSE)
 
-col_datetime <- output_daily[ , "datetime"]
+###################################3
+NEWmerged_data <-merged_data
 
-rep_datetime <- rep(col_datetime, num_rows/no_days) 
+NEWmerged_data$datetime <- as.Date(merged_data$datetime, format = "%m/%d/%Y")
 
-FLARE_dataset$datetime <- rep_datetime
+# Filter merged_data to only include data from 1st September 2023 onwards
+NEWmerged_data <- subset(NEWmerged_data, datetime >= as.Date("2023-09-01"))
 
-#Column 2: site_id##############################################################
 
-site_id <- c("feea")
+# Ensure datetime is in Date format
+NEWmerged_data$datetime <- as.Date(NEWmerged_data$datetime, format = "%m/%d/%Y")
 
-filling_site_id <- rep(site_id, lenght.out = num_rows)
+# Create a 'month_day' column to extract month and day from the 'datetime' column
+NEWmerged_data$month_day <- format(NEWmerged_data$datetime, "%m-%d")
 
-FLARE_dataset$site_id<- filling_site_id
+# Identify which rows are 1st September in any year
+NEWmerged_data$sep_first <- NEWmerged_data$month_day == "09-01"
 
-#Column 3: depth################################################################
+# Create a new cumulative temperature column based on 1st September resets
+NEWmerged_data$cumm_temp <- with(NEWmerged_data, ave(Temp_for_cumm_T, cumsum(sep_first), FUN = cumsum))
 
-rep_depth_values <- rep(c(0.9,2,5,8,11,14,16,18,20,22,27,32,42), each = no_days, times = 1) 
+# Start from the last row (Forecast_Cum_Temp)
+Forecast_Cum_Temp <- tail(NEWmerged_data, 1)
 
-FLARE_dataset$depth <- rep_depth_values
+# Generate 21 consecutive days starting from the next day of the last row's datetime
+new_dates <- seq(from = as.Date(Forecast_Cum_Temp$datetime) + 1, by = "days", length.out = 21)
 
-#Column 4: observation##########################################################
+# Create the new data for the 21 rows
+Forecast_Cum_Temp <- data.frame(
+  datetime = new_dates,
+  observation = rep(Forecast_Cum_Temp$observation, 21),
+  Temp_for_cumm_T = rep(Forecast_Cum_Temp$Temp_for_cumm_T, 21),
+  calendar_day = 1:21,  # Calendar day increments by 1
+  month_day = format(new_dates, "%m-%d"),  # Extract month and day
+  sep_first = rep(FALSE, 21),  # Not relevant, set to FALSE
+  cumm_temp = cumsum(rep(Forecast_Cum_Temp$Temp_for_cumm_T, 21)) + Forecast_Cum_Temp$cumm_temp  # Cumulative sum
+)
 
-values_0.9 <- output_daily[ , "0.9"]
-values_2 <- output_daily[ , "2"]
-values_4 <- output_daily[ , "5"]
-values_8 <- output_daily[ , "8"]
-values_11 <- output_daily[ , "11"]
-values_14 <- output_daily[ , "14"]
-values_16 <- output_daily[ , "16"]
-values_18 <- output_daily[ , "18"]
-values_20 <- output_daily[ , "20"]
-values_22 <- output_daily[ , "22"]
-values_27 <- output_daily[ , "27"]
-values_32 <- output_daily[ , "32"]
-values_42 <- output_daily[ , "42"]
+# View the result
+head(Forecast_Cum_Temp)
 
-observation_values  <- c(values_0.9, values_2, values_4, values_8, values_11, values_14, values_16, values_18,
-                         values_20,values_22,values_27, values_32, values_42)
+# Select the first and last columns from Forecast_Cum_Temp
+selected_columns <- Forecast_Cum_Temp[, c(1, ncol(Forecast_Cum_Temp))]
 
-observation_values[is.nan(observation_values)] <-  NA
 
-FLARE_dataset$observation <- observation_values 
+colnames(selected_columns)[1] <- "Forecast_date"
+colnames(selected_columns)[2] <- "Cummulative_SWT_in_degree_C"
 
-#Column 5: variable#############################################################
+# Add a third column "Forecast_generated_on" with the datetime of the first row
+selected_columns$Forecast_generated_on <- as.Date(Forecast_Cum_Temp$datetime[1])
 
-temp_variable <- c("temperature")
+# Create the file name with the desired format
+file_name <- paste("Last_forecast_generated_Feeagh_Cummulative_SWT.csv", sep = "")
 
-filling_variable <- rep(temp_variable, lenght.out = num_rows)
-
-FLARE_dataset$variable <- filling_variable
-
-#Updating the observations file for Feeagh######################################
-
-Past_feea_observations <- read.csv("Observations_feea.csv", header = TRUE)
-
-merged_observations <- bind_rows(FLARE_dataset, Past_feea_observations) %>%
-  arrange(depth)
-
-updated_observations <- distinct(merged_observations, datetime, depth, .keep_all = TRUE)
-
-updated_observations <- updated_observations %>% arrange(datetime) %>% arrange(depth)
-
-updated_observations$observation <- round(updated_observations$observation, 2)
-
-##Remove rows that are not in the same time format. These lines, however, sometimes only work when I access the code on my own and not through
-##a Github action. I believe that is not very important though.
-
- # time_format <- "\\d{4}-\\d{2}-\\d{2}$"
- # 
- # matching_rows <- grep(time_format, updated_observations$datetime)
- # 
- # if (length(matching_rows) == 0) {
- #   
- #   
- #   cat("time_format correction is not needed")
- #   
- #   
- # } else {
- #   
- #   matching_rows <- grep(time_format, updated_observations$datetime)
- #   
- #   updated_observations <- updated_observations[-matching_rows, ]
- #   
- #   cat("time_format corrected")
- # }
-
-##Remove rows that are not in the same timeformat ABOVE
-
-write.csv(updated_observations, "Observations_feea.csv", row.names = FALSE, quote = FALSE)
+# Save the selected columns to a CSV file
+write.csv(selected_columns, file = file_name, row.names = FALSE)
 
 #Last line######################################################################
-
